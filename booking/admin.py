60461cb1f1411edaf4793ceee3921c6aaa06295e
Laurent Andriamifidy <lolrenx@gmail.com>
2023-11-04 16:46:47 +0100

    add booking to admin
    a
    
    a

--- booking/admin.py
+++ booking/admin.py
@@ -1,1 +1,20 @@
 # Register your models here.
+from django.contrib import admin
+
+from booking.models import Booking
+
+
+class DropdownFilter(admin.AllValuesFieldListFilter):
+    template = "admin/dropdown_filter.html"
+
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "tenant",
+        "storage_box",
+        "start_date",
+        "end_date",
+    )
+    list_filter = (("storage_box__id", DropdownFilter),)
